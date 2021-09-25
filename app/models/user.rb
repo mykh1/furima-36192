ac7a6aa82828fe19cb924af251e08b4ec9222921
mykh1 <neige391124@yahoo.co.jp>
2021-09-25 21:12:30 +0900

    Userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,20 @@ class User < ApplicationRecord
   has_many :items
   has_many :order_histories
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_reading, presence: true
-  validates :first_name_reading, presence: true
-  validates :birthday, presence:true
+  with_options presence: true do
+   validates :nickname
+   validates :birthday 
+  end
+
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."} do
+    validates :last_name_reading
+    validates :first_name_reading
+  end
+
+ 
 end
